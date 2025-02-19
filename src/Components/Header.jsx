9212b8381d8ae7b557ca4c878ab1073ad25230bb
yossefmahmoud1bashنسختحرير <omaryosef136@gmail.com>
2025-02-19 21:24:54 +0200

    Fixed header hover issue and improved slider z-index

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,70 +1,49 @@
 import logo from './../assets/images/logo.png'
-import { HiHome,
-    HiMagnifyingGlass,
-    HiStar,
-    HiPlayCircle,
-    HiTv } from "react-icons/hi2";
-import { HiPlus,HiDotsVertical } from "react-icons/hi";
+import { HiHome, HiMagnifyingGlass, HiStar, HiPlayCircle, HiTv } from "react-icons/hi2";
+import { HiPlus, HiDotsVertical } from "react-icons/hi";
 import HeaderItem from './HeaderItem';
 import { useState } from 'react';
-import User_icon from "../assets/images/9.png"
+import User_icon from "../assets/images/9.png";
+
 function Header() {
-    const [toggle,setToggle]=useState(false);
-    const menu=[
-        {
-            name:'HOME',
-            icon:HiHome
-        },
-        {
-            name:'SEARCH',
-            icon:HiMagnifyingGlass
-        },
-        {
-            name:'WATCH LIST',
-            icon:HiPlus
-        },
-        {
-            name:'ORIGINALS',
-            icon:HiStar
-        },
-        {
-            name:'MOVIES',
-            icon:HiPlayCircle
-        },
-        {
-            name:'SERIES',
-            icon:HiTv
-        }
-    ]
-  return (
-    <div className='flex items-center justify-between p-[20px]'>
-        <div className='flex  gap-8 items-center'>
-        <img src={logo} className='w-[80px] 
-        md:w-[115px] object-cover' />
-        <div className='hidden md:flex gap-8'>
-        {menu.map((item ,index)=>(
-            <HeaderItem key={index} name={item.name} Icon={item.icon} />
-        ))}
+    const [toggle, setToggle] = useState(false);
+    const menu = [
+        { name: 'HOME', icon: HiHome },
+        { name: 'SEARCH', icon: HiMagnifyingGlass },
+        { name: 'WATCH LIST', icon: HiPlus },
+        { name: 'ORIGINALS', icon: HiStar },
+        { name: 'MOVIES', icon: HiPlayCircle },
+        { name: 'SERIES', icon: HiTv }
+    ];
+
+    return (
+        <div className='flex items-center justify-between p-[20px] bg-[#121212] relative z-50'>
+            <div className='flex gap-8 items-center'>
+                <img src={logo} className='w-[80px] md:w-[115px] object-cover' />
+                <div className='hidden md:flex gap-8'>
+                    {menu.map((item, index) => (
+                        <HeaderItem key={index} name={item.name} Icon={item.icon} />
+                    ))}
+                </div>
+                <div className='flex md:hidden gap-5'>
+                    {menu.map((item, index) => index < 3 && (
+                        <HeaderItem key={index} name={''} Icon={item.icon} />
+                    ))}
+                    <div className='md:hidden' onClick={() => setToggle(!toggle)}>
+                        <HeaderItem name={''} Icon={HiDotsVertical} />
+                        {toggle ? (
+                            <div className='absolute mt-3 bg-[#121212] border-[1px] border-gray-700 p-3 px-5 py-4'>
+                                {menu.map((item, index) => index > 2 && (
+                                    <HeaderItem key={index} name={item.name} Icon={item.icon} />
+                                ))}
+                            </div>
+                        ) : null}
+                    </div>
+                </div>
+            </div>
+            <img src={User_icon} className='w-[40px] rounded-full' />
         </div>
-          <div className='flex md:hidden gap-5'>
-        {menu.map((item,index)=>index<3&&(
-            <HeaderItem key={index} name={''} Icon={item.icon} />
-        ))}
-         <div className='md:hidden' onClick={()=>setToggle(!toggle)}>       
-            <HeaderItem name={''} Icon={HiDotsVertical} />
-           {toggle? <div className='absolute mt-3 bg-[#121212] 
-            border-[1px] border-gray-700 p-3 px-5 py-4'>
-            {menu.map((item,index)=>index>2&&(
-            <HeaderItem key={index} name={item.name} Icon={item.icon} />
-            ))}
-            </div>:null}
-            </div> 
-        </div>
-        </div>
-        <img src={User_icon}
-        className='w-[40px] rounded-full'/>
-    </div>
-  )
+    );
 }
 
-export default Header
+export default Header;
--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,61 +1,55 @@
 import { useEffect, useRef, useState } from "react";
 import GlobalApi from "../Services/GlobalApi";
-import { HiChevronLeft,HiChevronRight  } from "react-icons/hi";
-const IMAGE_BASE_URL= "https://image.tmdb.org/t/p/original";
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
 
 function Slider() {
-const screenfullwidth=window.innerWidth
-const [Movielist,setMovielist]=useState([])
-const element_refrennce=useRef()
-    
-useEffect(() => {
-    fetchDisneyMovies();
-}, []);
+    const screenfullwidth = window.innerWidth;
+    const [Movielist, setMovielist] = useState([]);
+    const element_refrennce = useRef();
 
-const sliderRight=(element)=>{
-    element.scrollLeft+=screenfullwidth-110
-}
-const sliderleft=(element)=>{
-    element.scrollLeft-=screenfullwidth-110
+    useEffect(() => {
+        fetchDisneyMovies();
+    }, []);
 
-}
+    const sliderRight = (element) => {
+        element.scrollLeft += screenfullwidth - 110;
+    };
+    const sliderleft = (element) => {
+        element.scrollLeft -= screenfullwidth - 110;
+    };
 
-const fetchDisneyMovies = () => {
-    GlobalApi.getDisneyMovies()
-        .then(response => {
-            setMovielist(response.data.results);
-        })
-        .catch(error => {
-            console.error("Error fetching Disney movies:", error);
-        });
-};
-
- 
-
+    const fetchDisneyMovies = () => {
+        GlobalApi.getDisneyMovies()
+            .then(response => {
+                setMovielist(response.data.results);
+            })
+            .catch(error => {
+                console.error("Error fetching Disney movies:", error);
+            });
+    };
 
     return (
-        <div>
-        <HiChevronLeft className="hidden md:block text-white text-[30px] absolute
-        mx-8 mt-[150px] cursor-pointer " onClick={()=>sliderleft(element_refrennce.current)}
-        />
-        <HiChevronRight className='hidden md:block text-white text-[30px] absolute
-        mx-8 mt-[150px] cursor-pointer right-0' onClick={()=>sliderRight(element_refrennce.current) }
-        />
+        <div className="relative z-10 overflow-hidden">
+            <HiChevronLeft className="hidden md:block text-white text-[30px] absolute
+            mx-8 mt-[150px] cursor-pointer " 
+            onClick={() => sliderleft(element_refrennce.current)} />
 
-   
-    <div className='flex overflow-x-auto w-full px-16 py-4
-    scrollbar-hide scroll-smooth 'ref={element_refrennce} > 
-        {Movielist.map((movie,index)=>(
-            <img key={index} src={IMAGE_BASE_URL + movie.backdrop_path}
-            className='min-w-full  md:h-[310px] aspect-ratio 
-            object-left-top mr-5 rounded-md hover:border-[4px]
-            border-gray-400 transition-all duration-100 ease-in'/>
-        ))}
-    </div>
-    </div>
+            <HiChevronRight className='hidden md:block text-white text-[30px] absolute
+            mx-8 mt-[150px] cursor-pointer right-0' 
+            onClick={() => sliderRight(element_refrennce.current)} />
+
+            <div className='flex overflow-x-auto w-full px-16 py-4 scrollbar-hide scroll-smooth' ref={element_refrennce}>
+                {Movielist.map((movie, index) => (
+                    <img key={index} src={IMAGE_BASE_URL + movie.backdrop_path}
+                        className='min-w-full md:h-[310px] aspect-ratio 
+                        object-left-top mr-5 rounded-md hover:border-[4px]
+                        border-gray-400 transition-all duration-100 ease-in' />
+                ))}
+            </div>
+        </div>
     );
-    
 }
 
 export default Slider;
